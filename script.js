3e74e8925e94f8f86f6f9ed445ed073af857fd6f
roufkhalid93 <roufkhalid93@gmail.com>
2024-11-28 18:08:23 +0800

    revamp projects and services sections

--- script.js
+++ script.js
@@ -3,21 +3,41 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
+//             })
+//         }
+//     })
+// }
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute;
+        let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active')
-            })
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            let link = document.querySelector('header nav a[href*="' + id + '"]');
+            if (link) {
+                link.classList.add('active');
+            }
         }
-    })
-}
+    });
+};
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
